d29e6b49e700c77017ac9dbf40dfe9f1e24d2ead
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-11-24 18:14:19 +0000

    prepare to add a sidebar creating sliderApp in Slider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,54 +1,17 @@
 import React from 'react'
-import Slide from './components/Slider/Slide'
 import '@csstools/normalize.css';
 import './styles/css/reset.css'
 import './styles/css/main.css'
 
-
-const slides = Array.from({length:50},()=> {
-  return {
-    title: "Title",
-    description: "Description",
-    image: 'https://source.unsplash.com/random?' + Math.floor(Math.random() * 100) + 1,
-  };
-})
-
-const initialState = {
-  slideIndex: 0
-};
-
-const slidesReducer = (state, event) => {
-  if (event.type === "NEXT") {
-    return {
-      ...state,
-      slideIndex: (state.slideIndex + 1) % slides.length
-    };
-  }
-  if (event.type === "PREV") {
-    return {
-      ...state,
-      slideIndex:
-        state.slideIndex === 0 ? slides.length - 1 : state.slideIndex - 1
-    };
-  }
-};
-
+// import components
+import SliderApp from './components/Slider/SliderApp'
 
 function App() {
-  const [state, dispatch] = React.useReducer(slidesReducer, initialState);
-
   return (
-    <div className="slides">
-      <button onClick={() => dispatch({ type: "PREV" })}>‹</button>
-
-      {[...slides, ...slides, ...slides].map((slide, i) => {
-        let offset = slides.length + (state.slideIndex - i);
-        return <Slide slide={slide} offset={offset} key={i} />;
-      })}
-      <button onClick={() => dispatch({ type: "NEXT" })}>›</button>
+    <div>
+      <SliderApp />
     </div>
-  );
+  )
 }
 
 export default App
-
