66ccbc1431eeca4992f152c8a99e58afcc82dc37
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-11-24 15:56:11 +0000

    add a more presentible front end

--- client/src/components/Slider/Slide.jsx
+++ client/src/components/Slider/Slide.jsx
@@ -1,97 +1,97 @@
 import React from "react";
 import { Button } from "../Button/Button";
+import { FaCheckCircle } from "react-icons/fa";
 
 console.clear();
 
-
-
 function useTilt(active) {
-  const ref = React.useRef(null);
+	const ref = React.useRef(null);
 
-  React.useEffect(() => {
-    if (!ref.current || !active) {
-      return;
-    }
+	React.useEffect(() => {
+		if (!ref.current || !active) {
+			return;
+		}
 
-    const state = {
-      rect: undefined,
-      mouseX: undefined,
-      mouseY: undefined
-    };
+		const state = {
+			rect: undefined,
+			mouseX: undefined,
+			mouseY: undefined,
+		};
 
-    let el = ref.current;
+		let el = ref.current;
 
-    const handleMouseMove = (e) => {
-      if (!el) {
-        return;
-      }
-      if (!state.rect) {
-        state.rect = el.getBoundingClientRect();
-      }
-      state.mouseX = e.clientX;
-      state.mouseY = e.clientY;
-      const px = (state.mouseX - state.rect.left) / state.rect.width;
-      const py = (state.mouseY - state.rect.top) / state.rect.height;
+		const handleMouseMove = (e) => {
+			if (!el) {
+				return;
+			}
+			if (!state.rect) {
+				state.rect = el.getBoundingClientRect();
+			}
+			state.mouseX = e.clientX;
+			state.mouseY = e.clientY;
+			const px = (state.mouseX - state.rect.left) / state.rect.width;
+			const py = (state.mouseY - state.rect.top) / state.rect.height;
 
-      el.style.setProperty("--px", px);
-      el.style.setProperty("--py", py);
-    };
+			el.style.setProperty("--px", px);
+			el.style.setProperty("--py", py);
+		};
 
-    el.addEventListener("mousemove", handleMouseMove);
+		el.addEventListener("mousemove", handleMouseMove);
 
-    return () => {
-      el.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, [active]);
+		return () => {
+			el.removeEventListener("mousemove", handleMouseMove);
+		};
+	}, [active]);
 
-  return ref;
+	return ref;
 }
-
 
 function Slide({ slide, offset }) {
-  const active = offset === 0 ? true : null;
-  const ref = useTilt(active);
+	const active = offset === 0 ? true : null;
+	const ref = useTilt(active);
 
-  return (
-    <div
-      ref={ref}
-      className="slide"
-      data-active={active}
-      style={{
-        "--offset": offset,
-        "--dir": offset === 0 ? 0 : offset > 0 ? 1 : -1
-      }}
-    >
-      <div
-        className="slideBackground"
-        style={{
-          backgroundImage: `url('${slide.image}')`
-        }}
-      />
-      <div
-        className="slideContent"
-        style={{
-          backgroundImage: `url('${slide.image}')`
-        }}
-      >
-        <div className="slideContentInner">
-          <h2 className="slideTitle"><Button
-        onClick={() => {
-          console.log("You Clicked on Me!");
-        }}
-        type="button"
-        buttonStyle="btn--primary--outline"
-        buttonSize="btn--large"
-      >
-        Buy Now
-      </Button></h2>
-          <h3 className="slideSubtitle">{slide.subtitle}</h3>
-          <p className="slideDescription">{slide.description}</p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			ref={ref}
+			className="slide"
+			data-active={active}
+			style={{
+				"--offset": offset,
+				"--dir": offset === 0 ? 0 : offset > 0 ? 1 : -1,
+			}}
+		>
+			<div
+				className="slideBackground"
+				style={{
+					backgroundImage: `url('${slide.image}')`,
+				}}
+			/>
+			<div
+				className="slideContent"
+				style={{
+					backgroundImage: `url('${slide.image}')`,
+				}}
+			>
+				<div className="slideContentInner">
+				
+						<Button
+            className="slideTitle"
+							onClick={() => {
+								console.log("You Clicked on Me!");
+							}}
+							type="button"
+							buttonStyle="btn--danger--solid"
+							buttonSize="btn--large"
+						>
+							Like
+							<FaCheckCircle id="checkMark"/>
+						</Button>
+					<h3 className="slideSubtitle">{slide.subtitle}</h3>
+					<p className="slideDescription">{slide.description}</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Slide
-
+export default Slide;
