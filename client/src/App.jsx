f7b95129499c47b39143097397f97a6eddccc3e8
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-11-24 13:19:11 +0000

    add a working well styled image shaffler

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
-import Slider from './components/Slider/Slider'
+import Slide from './components/Slider/Slide'
+import './styles/css/main.css'
+
+
+const slides = Array.from({length:50},()=> {
+  return {
+    title: "Title",
+    description: "Description",
+    image: 'https://source.unsplash.com/random?' + Math.floor(Math.random() * 100) + 1,
+  };
+})
+
+const initialState = {
+  slideIndex: 0
+};
+
+const slidesReducer = (state, event) => {
+  if (event.type === "NEXT") {
+    return {
+      ...state,
+      slideIndex: (state.slideIndex + 1) % slides.length
+    };
+  }
+  if (event.type === "PREV") {
+    return {
+      ...state,
+      slideIndex:
+        state.slideIndex === 0 ? slides.length - 1 : state.slideIndex - 1
+    };
+  }
+};
+
 
 function App() {
+  const [state, dispatch] = React.useReducer(slidesReducer, initialState);
+
   return (
-    <div>App</div>
-  )
+    <div className="slides">
+      <button onClick={() => dispatch({ type: "PREV" })}>‹</button>
+
+      {[...slides, ...slides, ...slides].map((slide, i) => {
+        let offset = slides.length + (state.slideIndex - i);
+        return <Slide slide={slide} offset={offset} key={i} />;
+      })}
+      <button onClick={() => dispatch({ type: "NEXT" })}>›</button>
+    </div>
+  );
 }
 
 export default App
