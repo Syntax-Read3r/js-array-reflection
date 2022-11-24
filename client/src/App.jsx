6cacda334a370985b98e0449587470736875d565
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-11-24 18:23:57 +0000

    add a template for your app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import '@csstools/normalize.css';
 import './styles/css/reset.css'
 import './styles/css/main.css'
 
 // import components
+import Sidebar from './components/Sidebar/Sidebar'
 import SliderApp from './components/Slider/SliderApp'
+import Analytics from './components/Pages/Analytics/Analytics'
+import Profiles from './components/Pages/Profiles/Profiles'
 
 function App() {
   return (
-    <div>
-      <SliderApp />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SliderApp />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/profiles" element={<Profiles />} />
+
+
+        </Routes>
+    </BrowserRouter>
   )
 }
 
